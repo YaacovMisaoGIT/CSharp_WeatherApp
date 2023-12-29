2c89ce3a2c0fdfab00bcb36dcdd88dc61c9d2d5d
yaacovmisao747 <yaacovmisao@gmail.com>
2023-12-29 10:11:04 +0200

    first commit

---
+++ StudentHttpApi/StudentClass.cs
@@ -0,0 +1,15 @@
+﻿namespace StudentHttpApi
+{
+    public class StudentClass
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public StudentClass(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;  
+        }
+
+    }
+}
